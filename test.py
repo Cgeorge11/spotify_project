4bd9f691ec287ef673c0fb349dc58021b6066b28
Cylin George <g.cylin1@gmail.com>
2022-03-21 21:26:31 -0400

    functions added

--- test.py
+++ test.py
@@ -7,18 +7,31 @@ import requests
 
 
 
-# Spotify App credentials 
+# Spotify App credentials
 cid = app_id
 secret = app_secret 
 
+# Access to general Spotify data. If user doesn't authorize app to access profile this will allow user to still access some features 
+client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
+spot = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
+
+
 #Authorization from user to access profile specific information
-spot = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cid,
     client_secret=secret,
     redirect_uri="http://localhost:8080",
     scope="user-library-read"))
-# Access to general Spotify data. If user doesn't authorize app to access profile this will allow user to still access some features 
-client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 
-print('end of program')
+# Get Spotify catalog information about albums, artists, tracks that match keyword string
+def search_artist ():
+    pass 
+
+def search_track():
+    pass
+
+def search_albums():
+    pass 
+
+
+print('wow') 
